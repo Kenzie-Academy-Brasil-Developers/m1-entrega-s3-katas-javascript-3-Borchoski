13605436f12aac3b29250fb5a705252d37898350
Joao Borchoski <joaohenriqueborchoski>
2022-08-24 17:25:16 -0300

    entrega

--- katas3.js
+++ katas3.js
@@ -1,97 +1,199 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata1() {
-    // implemente o código do kata 1 aqui
+    let contador = [];
+    for (let i = 1; i <=25; i++) {
+        contador.push(i)
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata1())
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let contador = [];
+    for (let i = 25; i >= 1; i--) {
+        contador.push(i)
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata2())
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+    let contador = []
+    for(let i = -1; i >= -25; i--){
+        contador.push(i)
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata3())
 
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let contador = []
+    for(let i = -25; i <= -1; i++){
+        contador.push(i)
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata4())
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let contador = []
+    for(let i = 25; i >= -25; i--){
+        if(i % 2 == 1 || i % 2 == -1){
+            contador.push(i)
+        }   
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata5())
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+    let contador = []
+    for(let i = 1; i <= 100; i++){
+        if(i % 3 == 0 ){
+            contador.push(i)
+        }   
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata6())
 
 function kata7() {
-    // implemente o código do kata 7 aqui
+    let contador = []
+    for(let i = 0; i <= 100 ; i++){
+        if(i % 7 == 0 ){
+            contador.push(i)
+        }   
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata7())
 
 function kata8() {
-    // implemente o código do kata 8 aqui
+    let contador = []
+    for(let i = 100; i >= 1; i--){
+        if(i % 7 == 0 || i % 3 == 0){
+            contador.push(i)
+        }   
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata8())
 
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let contador = []
+    for(let i = 1; i <= 100; i++){
+        if(i % 2 == 1 && i % 5 == 0){
+            contador.push(i)
+        }   
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata9())
 
-function kata10() {
-    // implemente o código do kata 10 aqui
+function kata10(lista) {
+    let contador = []
+    for(let i = 0; i < lista.length; i++){
+        contador.push(lista[i])
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata10(sampleArray))
 
-function kata11() {
-    // implemente o código do kata 11 aqui
+function kata11(lista) {        
+    let contador = []
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i] % 2 == 0){
+            contador.push(lista[i])
+        }
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata11(sampleArray))
 
-function kata12() {
-    // implemente o código do kata 12 aqui
+function kata12(lista) {
+    let contador = []
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i] % 2 == 1){
+            contador.push(lista[i])
+        }
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata12(sampleArray))
 
-function kata13() {
-    // implemente o código do kata 13 aqui
+function kata13(lista) {
+    let contador = []
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i] % 8 == 0){
+            contador.push(lista[i])
+        }
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata13(sampleArray))
 
-function kata14() {
-    // implemente o código do kata 14 aqui
+function kata14(lista) {
+    let contador = []
+    for(let i = 0; i < lista.length; i++){
+        contador.push(lista[i] * lista[i])
+    }
+    let contator2 = contador.join(", ")
+    return contator2
 }
+console.log(kata14(sampleArray))
 
 function kata15() {
-    // implemente o código do kata 15 aqui
+    let contador = 0
+    for(let i = 1; i <= 20; i++){
+        contador += i
+    }
+    return contador
 }
+console.log(kata15())
 
-function kata16() {
-    // implemente o código do kata 16 aqui
+function kata16(lista) {
+    let contador = 0
+    for(let i = 0; i < lista.length; i++){
+        contador += lista[i]
+    }
+    return contador
 }
+console.log(kata16(sampleArray))
 
-function kata17() {
-    // implemente o código do kata 17 aqui
+function kata17(lista) {
+    let contador = lista[0]
+    for(let i = 1; i < lista.length; i++){
+        let variavel = lista[i]
+        if(variavel < contador){
+            contador = variavel
+        }
+    }
+    return contador
 }
+console.log(kata17(sampleArray))
 
-function kata18() {
-    // implemente o código do kata 18 aqui
+function kata18(lista) {
+    let contador = lista[0]
+    for(let i = 1; i < lista.length; i++){
+        let variavel = lista[i]
+        if(variavel > contador){
+            contador = variavel
+        }
+    }
+    return contador
 }
-
-/**
- * Daqui em diante são os bônus, por sua conta e risco
- */
-
-function kataBonus1() {
-    // implemente o código do kata bonus 1 aqui
-}
-
-function kataBonus2() {
-    // implemente o código do kata bonus 2 aqui
-}
-
-function kataBonus3() {
-    // implemente o código do kata bonus 3 aqui
-}
-
-function kataBonus4() {
-    // implemente o código do kata bonus 4 aqui
-}
-
-function kataBonus5() {
-    // implemente o código do kata bonus 5 aqui
-}
+console.log(kata18(sampleArray))
